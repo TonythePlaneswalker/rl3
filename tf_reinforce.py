48ef5a1c186ed7d57c9ffe7846bdae27e99d4752
wwenli <iamwenliwang@gmail.com>
2018-04-07 15:26:20 -0400

    Added Tensorborad visualization

--- tf_reinforce.py
+++ tf_reinforce.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 import numpy as np
@@ -21,6 +22,7 @@ class Reinforce(object):
         self.env = env
         self.state_dim = env.observation_space.shape[0]
         self.num_actions = env.action_space.n
+        self.log_dir = args.log_dir
 
         self.state = tf.placeholder(tf.float32, shape=[None, self.state_dim])
         self.action = tf.placeholder(tf.float32, [None, self.num_actions])
@@ -50,38 +52,35 @@ class Reinforce(object):
         config.gpu_options.allow_growth = True
         config.allow_soft_placement = True
         sess = tf.Session(config=config)
-
-        # Create summaries
-        merged = tf.summary.merge_all()
+        global_step = tf.Variable(0, trainable=False, name='global_step')
 
         # Summary for tensorboard
-        # loss_summary = tf.summary.scalar('loss', self.loss)
-        # episode_length = tf.placeholder(tf.int32, shape=(), name='episode_length')
-        # length_summary = tf.summary.scalar('episode_length', episode_length)
-        # avg_reward = tf.placeholder(tf.float32, shape=(), name='avg_reward')
-        # avg_reward_summary = tf.summary.scalar('average reward', avg_reward)
-        # std_reward = tf.placeholder(tf.float32, shape=(), name='std_reward')
-        # std_reward_summary = tf.summary.scalar('std reward', std_reward)
-        #
-        # # Set up saving and logging
-        # saver = tf.train.Saver(max_to_keep=10)
-        # save_path = os.path.join(args.log_dir, 'checkpoints', 'model')
-        # steps_per_save = args.max_iter // 9
+        loss_summary = tf.summary.scalar('loss', self.loss)
+        episode_length = tf.placeholder(tf.int32, shape=(), name='episode_length')
+        length_summary = tf.summary.scalar('episode_length', episode_length)
+        avg_reward = tf.placeholder(tf.float32, shape=(), name='avg_reward')
+        avg_reward_summary = tf.summary.scalar('average reward', avg_reward)
+        std_reward = tf.placeholder(tf.float32, shape=(), name='std_reward')
+        std_reward_summary = tf.summary.scalar('std reward', std_reward)
+
+        # Set up saving and logging
+        saver = tf.train.Saver(max_to_keep=10)
+        save_path = os.path.join(self.log_dir, 'checkpoints', 'model')
+        steps_per_save = self.num_episodes // 9
 
         sess.run(tf.global_variables_initializer())
 
-        # if os.path.exists(args.log_dir):
-        #     delete_key = input('%s exists. Delete? [y (or enter)/N]' % args.log_dir)
-        #     if delete_key == 'y' or delete_key == "":
-        #         os.system('rm -rf %s/*' % args.log_dir)
-        # os.makedirs(os.path.join(args.log_dir, 'checkpoints'), exist_ok=True)
-        # saver.save(self.sess, save_path, global_step)
-        # writer = tf.summary.FileWriter('./log/%s'.format(args.log_dir), self.sess.graph)
+        if os.path.exists(self.log_dir):
+            delete_key = input('{} exists. Delete? [y (or enter)/N]'.format(self.log_dir))
+            if delete_key == 'y' or delete_key == "":
+                os.system('rm -rf {}/*'.format(self.log_dir))
+        os.makedirs(os.path.join(self.log_dir, 'checkpoints'))
+        saver.save(sess, save_path, global_step)
+        writer = tf.summary.FileWriter('./log/{}'.format(self.log_dir), sess.graph)
 
         for i in range(self.num_episodes):
             states, actions, rewards = self.generate_episode(env, sess)
             T = len(rewards)
-            # print(T)
             G = np.zeros(T, dtype=np.float32)
 
             # compute Gt
@@ -90,20 +89,29 @@ class Reinforce(object):
                     G[t] += gamma**(k - t) * rewards[k]
 
             # train the model
-            _, cur_policy, cur_loss, cur_summaries = sess.run([self.train_op, self.policy, self.loss, merged],
+            _, cur_policy, cur_loss, cur_summaries = sess.run([self.train_op, self.policy, self.loss, loss_summary],
                                                               feed_dict={self.state: states,
                                                                          self.action: actions,
                                                                          self.G: G})
-            # print("Loss: {}".format(cur_loss))
-            # train_writer.add_summary(cur_summaries)
+            writer.add_summary(cur_summaries, i)
+            summary = sess.run(length_summary, feed_dict={episode_length: T})
+            writer.add_summary(summary, i)
+
             if (i + 1) % 100 == 0:
                 cum_rewards = np.zeros(self.eval_episodes)
                 for j in range(self.eval_episodes):
                     rewards = self.generate_episode(self.env, sess)[2]
                     cum_rewards[j] = np.sum(rewards)
+                summary = sess.run(avg_reward_summary, feed_dict={avg_reward: cum_rewards.mean()})
+                writer.add_summary(summary, i)
+                summary = sess.run(std_reward_summary, feed_dict={std_reward: cum_rewards.std()})
+                writer.add_summary(summary, i)
                 print("Training Episode: %f Cumulative rewards: mean: %f std: %f" % (i + 1, cum_rewards.mean(),
                                                                                      cum_rewards.std()))
                 print("Loss: %f" % cur_loss)
+
+            if i % steps_per_save == 0:
+                saver.save(sess, save_path, global_step)
 
     def select_action(self, state, sess):
         """
@@ -166,6 +174,7 @@ class Reinforce(object):
 def parse_arguments():
     # Command-line flags are defined here.
     parser = argparse.ArgumentParser()
+    parser.add_argument('--log_dir', type=str, help='log directory where the checkpoints and summaries are saved.')
     parser.add_argument('--num-episodes', dest='num_episodes', type=int, default=50000,
                         help="Number of episodes to train on.")
     parser.add_argument('--lr', dest='lr', type=float, default=0.001, help="The learning rate.")
